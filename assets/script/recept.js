030151ca0a3c01d36afa8ab8e73d3e128a46d1c5
marha784 <marcus.peter.hallen@gmail.com>
2017-02-26 18:13:55 +0100

    local storage

--- assets/script/recept.js
+++ assets/script/recept.js
@@ -1,31 +1,41 @@
 
 var baseIngr = [];
 
+function setLocalStorage(){
+    if(typeof Storage !== "undefined")
+    {
+       // LocalStorage and sessionStorage support!
+      if (window.localStorage.getItem("cakeCount")) {
+            var localCakeCount;
+            localCakeCount = document.getElementById("piecesOfCake").value;
+            window.localStorage.setItem("cakeCount", localCakeCount);
+        } else {
+            window.localStorage.setItem("cakeCount", 15);
+        }
+    }
+}
+
 function updateRecipe() {
 
     "use strict";
-    var i, numCakes, asideElement, ingrList;
-    numCakes= document.getElementById("numCakes").value/15;
+    var i, constant, content_left, ingList;
+    constant= document.getElementById("piecesOfCake").value/15;
     
-    asideElement = document.getElementsByClassName("content_left")[0];
-    ingrList = asideElement.getElementsByTagName("li");
-    for (i = 0; i < ingrList.length; i++) {
-        var tempStr = ingrList[i].textContent; 
+    content_left= document.getElementsByClassName("content_left")[0];
+    ingList= content_left.getElementsByTagName("li");
+    for (i = 0; i < ingList.length; i++) {
+        var tempStr = ingList[i].textContent; 
         var tempNumber = parseInt(tempStr.match(/[0-9]+/));
         console.log(tempNumber);
         if (typeof (baseIngr[i]) === "undefined") {
             baseIngr[i] = tempNumber;
         }
-        tempNumber = baseIngr[i] * numCakes;
+        tempNumber = baseIngr[i] * constant;
         tempStr = tempStr.replace(/[0-9]+/, tempNumber);
-        ingrList[i].textContent = tempStr;
+        ingList[i].textContent = tempStr;
     }
+    setLocalStorage();
 }
-
-window.oninput = function () {
-    "use strict";
-updateRecipe();
-};
 
 function updateTextInput(val) {
  if(val===" "){
@@ -33,3 +43,24 @@ function updateTextInput(val) {
  }
  document.getElementById("textInput").value=val; 
 }
+
+
+
+window.oninput = function () {
+    "use strict";
+updateRecipe();
+};
+
+
+window.onload = function () {
+    "use strict";
+  
+    if (typeof (Storage) !== "undefined") {
+        //Store the cakeamount 
+        if (window.localStorage.getItem("cakeCount")) {
+            document.getElementById("piecesOfCake").value = parseInt(window.localStorage.getItem("cakeCount"));
+            updateRecipe();
+            document.getElementById("textInput").value=document.getElementById("piecesOfCake").value;
+        }
+    }
+};
